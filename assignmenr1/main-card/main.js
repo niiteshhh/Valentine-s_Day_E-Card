8932451ea43f754e46fa4929e73bb80a913714a6
niiteshhh <150144581+niiteshhh@users.noreply.github.com>
2024-06-13 10:59:04 +0800

    Add files via upload

---
+++ assignmenr1/main-card/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  $(".happy-valentines")
+    .mouseenter(function () {
+      $(".valentines-day-card").stop().animate(
+        {
+          top: "-140px",
+          height: "0px",
+        },
+        1000
+      );
+    })
+    .mouseenter(function () {
+      $(".valentines-day-card-back").stop().animate(
+        {
+          top: "300px",
+          height: "300px"
+        },
+        1000
+      );
+    })
+    .mouseleave(function () {
+      $(".card").stop().animate(
+        {
+          top: 0,
+        },
+        "slow"
+      );
+    });
+
+});
+
+window.onload = function () {
+  let params = new URLSearchParams(window.location.search);
+  let message = params.get('message');
+
+  if (message) {
+    document.getElementById('message').value = decodeURIComponent(message);
+    document.getElementById('message').disabled = true;
+    document.getElementById('generateLink').style.display = 'none';
+    document.getElementById('copyLink').style.display = 'none';
+  }
+};
+
+document.getElementById('generateLink').addEventListener('click', function () {
+  let message = encodeURIComponent(document.getElementById('message').value);
+  let shareableLink = `http://localhost?message=${message}`;
+  document.getElementById('link').value = shareableLink;
+});
+
+document.getElementById('copyLink').addEventListener('click', function () {
+  let copyText = document.getElementById('link');
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+  alert('Copied the text: ' + copyText.value);
+});
